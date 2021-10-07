43abaa02ae060cdacd128d88e1595d44d6f022cc
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 21:13:24 +0530

    Initial commit

---
+++ main.sql
@@ -0,0 +1,12 @@
+***Top SQL Project Ideas For Beginners***
+
+1. Library Management System. 
+2. Centralized College Database. 
+3. Student Database Management. 
+4. Online Retail Application Database. 
+5. Inventory Control Management. 
+6. Hospital Management System.
+7. Railway System Database. 
+8. Payroll Management System.
+9. An SMS-based Remote Server Monitoring System.
+10. Blood Donation Database.
